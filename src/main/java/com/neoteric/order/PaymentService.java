4418055d8894e4b07041c828ead453b66a8789b8
Naveendevanaboina <devanaboinanaveen2@gmail.com>
2025-08-23 09:10:21 +0530

    second commit

--- src/main/java/com/neoteric/order/PaymentService.java
+++ src/main/java/com/neoteric/order/PaymentService.java
@@ -1,5 +1,6 @@
 package com.neoteric.order;
 
+import java.util.Date;
 
 public class PaymentService {
 
@@ -18,15 +19,21 @@ public class PaymentService {
             throw new InsufficientBalanceException("Insufficient balance! Available: " + accountBalance);
         }
 
-
         accountBalance -= amount;
 
         String txnId = "TXN" + System.currentTimeMillis();
         Payment payment = new Payment(amount, method, txnId, orderId);
 
+
+        payment.setPaymentDate(new Date());
+
+
         System.out.println("Payment Successful | Amount: " + amount +
-                " | Method: " + method + " | TxnId: " + txnId +
+                " | Method: " + method +
+                " | TxnId: " + txnId +
+                " | Date: " + payment.getPaymentDate() +
                 " | Remaining Balance: " + accountBalance);
+
         return payment;
     }
 }
